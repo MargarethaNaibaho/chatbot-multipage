b99b3408407d8ccf2d918912fa70bc0ff6b38d70
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2025-05-13 17:55:38 +0700

    feat:initialize pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,27 @@
 import streamlit as st
 
-st.title("Hello everyone!")
-st.text("Ini mau mulai untuk capstone project LaskarAI nih")
+# Page setup
+about_page = st.Page(
+    page="views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True
+)
+
+sales_dashboard_page = st.Page(
+    page="views/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+
+chatbot_page = st.Page(
+    page="views/chatbot.py",
+    title="Chatbot",
+    icon=":material/smart_toy:"
+)
+
+# Navigation setup
+pg = st.navigation(pages=[about_page, sales_dashboard_page, chatbot_page])
+
+# Run
+pg.run()
